4ad2921d766ec45acead9a5541fc48db21dad58d
jr <jhanarato@gmail.com>
2025-07-22 10:20:31 +0800

    Construct request from query.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ struct SearchTestCase {
     query: String,
 }
 
+struct SearchRequest {
+    uri: String,
+}
+
+fn create_request(test_case: SearchTestCase) -> SearchRequest {
+    let uri = format!(
+        "http://localhost/api/search/instant?limit=50&query={}&language=en&restrict=all&matchpartial=false",
+        test_case.query
+    );
+    SearchRequest { uri }
+}
+
 fn main() {
     println!("Run system tests on the SuttaCentral search engine.");
 }
@@ -17,5 +29,18 @@ mod tests {
         };
 
         assert_eq!(case.query, "adze")
+    }
+
+    #[test]
+    fn can_add_query_to_request() {
+        let case = SearchTestCase {
+            query: String::from("adze"),
+        };
+
+        let request = create_request(case);
+        assert_eq!(
+            request.uri,
+            "http://localhost/api/search/instant?limit=50&query=adze&language=en&restrict=all&matchpartial=false"
+        )
     }
 }
