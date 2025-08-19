adbf31ac06508841421953b502a49491f47ce5eb
jr <jhanarato@gmail.com>
2025-08-19 10:55:59 +0800

    Rename struct field so Serde can find it.

--- src/suite.rs
+++ src/suite.rs
@@ -25,7 +25,7 @@ pub struct TestCase {
 #[serde(rename_all = "kebab-case")]
 pub struct TestSuite {
     settings: Settings,
-    // test_cases: Vec<TestCase>,
+    test_case: Vec<TestCase>,
 }
 
 #[cfg(test)]
@@ -61,6 +61,11 @@ mod tests {
                 selected_languages: vec!["en".to_string(), "pli".to_string()],
                 match_partial: false,
             },
+            test_case: vec![TestCase {
+                description: Some("Search for the metta sutta in English and Pali".to_string()),
+                query: "metta".to_string(),
+                selected_languages: Some(vec!["pli".to_string(), "en".to_string()]),
+            }],
         };
 
         assert_eq!(suite, expected);
