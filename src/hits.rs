2236db18e78e87b62611a4a4308ad0356500ce59
jr <jhanarato@gmail.com>
2025-09-13 10:20:25 +0800

    Reorganise.

--- src/hits.rs
+++ src/hits.rs
@@ -3,26 +3,19 @@ mod tests {
     use serde::Deserialize;
 
     #[derive(Deserialize, Debug)]
-    struct TopLevel {
-        numbers: Numbers,
-    }
-
-    #[derive(Deserialize, Debug)]
-    struct Numbers {
-        list: Vec<i32>,
+    struct Unsorted {
+        numbers: Vec<u32>,
     }
 
     #[test]
     fn parse_all_numbers() {
         let json = r#"
         {
-            "numbers": {
-                "list": [1, 2, 3, 4, 5]
-            }
+            "numbers": [1, 2, 3, 4, 5]
         }
         "#;
 
-        let top_level = serde_json::from_str::<TopLevel>(json).unwrap();
-        assert_eq!(top_level.numbers.list, vec![1, 2, 3, 4, 5])
+        let top_level = serde_json::from_str::<Unsorted>(json).unwrap();
+        assert_eq!(top_level.numbers, vec![1, 2, 3, 4, 5])
     }
 }
