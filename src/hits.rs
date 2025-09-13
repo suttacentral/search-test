11203c9f605b989dca8290914a457a749d7b8e4b
jr <jhanarato@gmail.com>
2025-09-13 10:23:57 +0800

    Experiment: extract json() function and rename to "unsorted"

--- src/hits.rs
+++ src/hits.rs
@@ -7,15 +7,13 @@ mod tests {
         numbers: Vec<u32>,
     }
 
-    #[test]
-    fn parse_all_numbers() {
-        let json = r#"
-        {
-            "numbers": [1, 2, 3, 4, 5]
-        }
-        "#;
+    fn json() -> String {
+        String::from(r#" { "numbers": [1, 2, 3, 4, 5] } "#)
+    }
 
-        let top_level = serde_json::from_str::<Unsorted>(json).unwrap();
-        assert_eq!(top_level.numbers, vec![1, 2, 3, 4, 5])
+    #[test]
+    fn parse_unsorted() {
+        let unsorted = serde_json::from_str::<Unsorted>(json().as_str()).unwrap();
+        assert_eq!(unsorted.numbers, vec![1, 2, 3, 4, 5])
     }
 }
