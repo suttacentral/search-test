4182c3ca3326d73ef071920a5150f4d4a3a39f9b
jr <jhanarato@gmail.com>
2025-09-13 10:52:23 +0800

    Remove experiment.

--- src/hits.rs
+++ src/hits.rs
@@ -1,19 +1,1 @@
-#[cfg(test)]
-mod tests {
-    use serde::Deserialize;
 
-    #[derive(Deserialize, Debug)]
-    struct Unsorted {
-        numbers: Vec<u32>,
-    }
-
-    fn json() -> String {
-        String::from(r#" { "numbers": [1, 2, 3, 4, 5] } "#)
-    }
-
-    #[test]
-    fn parse_unsorted() {
-        let unsorted = serde_json::from_str::<Unsorted>(json().as_str()).unwrap();
-        assert_eq!(unsorted.numbers, vec![1, 2, 3, 4, 5])
-    }
-}
