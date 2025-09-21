5424e43a04b76b2fe0e5fe01a4954ad2a6c40aec
jr <jhanarato@gmail.com>
2025-09-21 10:35:59 +0800

    Sketch out test running.

--- src/run.rs
+++ src/run.rs
@@ -1,7 +1,9 @@
 use crate::test_case::TestCase;
 use crate::test_suite::TestSuite;
 
-use anyhow::Result;
+use crate::request::build;
+use crate::response::SearchResponse;
+use anyhow::{Context, Result};
 
 pub struct Runner {
     suite: TestSuite,
@@ -17,12 +19,31 @@ impl Runner {
     }
 
     pub fn run(&self) -> impl Iterator<Item = TestResult> {
-        self.suite.test_cases().map(Self::run_test)
+        self.suite
+            .test_cases()
+            .map(|test_case| self.run_test(test_case))
     }
 
-    fn run_test(test_case: Result<TestCase>) -> TestResult {
+    fn send(endpoint: String, test_case: TestCase) -> Result<SearchResponse> {
+        let response = build(endpoint, test_case).send()?;
+        response.json().context("Could not get JSON from response")
+    }
+
+    fn run_test(&self, test_case: Result<TestCase>) -> TestResult {
         match test_case {
-            Ok(_test_case) => TestResult { passed: true },
+            Ok(test_case) => {
+                let response = Self::send(self.suite.endpoint(), test_case);
+                match response {
+                    Ok(response) => {
+                        println!("{response}");
+                        TestResult { passed: true }
+                    }
+                    Err(error) => {
+                        println!("{error:?}");
+                        TestResult { passed: false }
+                    }
+                }
+            }
             Err(error) => {
                 println!("{error:?}");
                 TestResult { passed: false }
