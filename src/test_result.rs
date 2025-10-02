3e4d016d0e5d2d1bea507fbe50678b8067241963
jr <jhanarato@gmail.com>
2025-10-02 18:12:51 +0800

    Implement CategorySearch as enum.

--- src/test_result.rs
+++ src/test_result.rs
@@ -1,5 +1,5 @@
 use crate::expected::Expected;
-use crate::identifiers::SearchResultKey;
+use crate::identifiers::{DictionaryUrl, SearchResultKey, SuttaplexUid, TextUrl};
 use crate::response::SearchResults;
 use crate::search_service::TimedSearchResults;
 use crate::test_case::TestCase;
@@ -64,27 +64,32 @@ impl Outcome {
     }
 }
 
-struct CategorySearch<C>
-where
-    C: PartialEq,
-{
-    id: C,
-    sequence: Vec<C>,
+#[derive(Debug, PartialEq)]
+enum CategorySearch {
+    Text {
+        search_for: TextUrl,
+        in_sequence: Vec<TextUrl>,
+    },
+    Dictionary {
+        search_for: DictionaryUrl,
+        in_sequence: Vec<DictionaryUrl>,
+    },
+    Suttaplex {
+        search_for: SuttaplexUid,
+        in_sequence: Vec<SuttaplexUid>,
+    },
 }
 
-impl<C: PartialEq> CategorySearch<C> {
-    fn found(&self) -> Outcome {
-        match self.sequence.contains(&self.id) {
-            true => Outcome::Found,
-            false => Outcome::NotFound,
+impl CategorySearch {
+    fn new(key: &SearchResultKey, results: &SearchResults) -> Self {
+        match key {
+            SearchResultKey::Suttaplex { uid } => Self::Suttaplex {
+                search_for: uid.clone(),
+                in_sequence: results.suttaplex.iter().cloned().collect(),
+            },
+            SearchResultKey::Dictionary { url } => todo!(),
+            SearchResultKey::Text { url } => todo!(),
         }
-    }
-
-    fn rank(&self) -> Option<usize> {
-        self.sequence
-            .iter()
-            .position(|hit| hit == &self.id)
-            .map(|position| position + 1)
     }
 }
 
@@ -99,27 +104,26 @@ mod tests {
     use std::time::Duration;
 
     #[test]
-    fn suttaplex_found() {
-        let search = CategorySearch {
-            id: SuttaplexUid::from("mn1"),
-            sequence: vec![SuttaplexUid::from("mn1")],
+    fn create_suttaplex_search_result() {
+        let key = SearchResultKey::Suttaplex {
+            uid: SuttaplexUid::from("mn1"),
         };
 
-        assert_eq!(search.found(), Outcome::Found);
-    }
-
-    #[test]
-    fn suttaplex_ranked() {
-        let search = CategorySearch {
-            id: SuttaplexUid::from("mn2"),
-            sequence: vec![
-                SuttaplexUid::from("mn1"),
-                SuttaplexUid::from("mn2"),
-                SuttaplexUid::from("mn3"),
-            ],
+        let search_results = SearchResults {
+            text: Vec::new(),
+            dictionary: Vec::new(),
+            suttaplex: vec![SuttaplexUid::from("mn1")],
         };
 
-        assert_eq!(search.rank(), Some(2));
+        let search = CategorySearch::new(&key, &search_results);
+
+        assert_eq!(
+            search,
+            CategorySearch::Suttaplex {
+                search_for: SuttaplexUid::from("mn1"),
+                in_sequence: vec![SuttaplexUid::from("mn1")]
+            }
+        )
     }
 
     fn test_case() -> TestCase {
