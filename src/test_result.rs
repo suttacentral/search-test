ce10ce35b75d17ac79b52e8e15ad288ab624d916
jr <jhanarato@gmail.com>
2025-10-02 15:50:24 +0800

    Create generic CategorySearch

--- src/test_result.rs
+++ src/test_result.rs
@@ -16,3 +16,33 @@ impl TestResult {
         }
     }
 }
+
+struct CategorySearch<C>
+where
+    C: PartialEq,
+{
+    id: C,
+    sequence: Vec<C>,
+}
+
+impl<C: PartialEq> CategorySearch<C> {
+    fn found(&self) -> bool {
+        self.sequence.contains(&self.id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::identifiers::SuttaplexUid;
+    use crate::test_result::CategorySearch;
+
+    #[test]
+    fn suttaplex_found() {
+        let search = CategorySearch {
+            id: SuttaplexUid::from("mn1"),
+            sequence: vec![SuttaplexUid::from("mn1")],
+        };
+
+        assert!(search.found())
+    }
+}
