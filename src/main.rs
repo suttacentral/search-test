6bc3f99311cce7b7c8a98aa3dbea29f944b576e9
jr <jhanarato@gmail.com>
2025-10-12 12:10:12 +0800

    Extract run_application()

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,28 @@ mod test_suite;
 
 use crate::file_load::load_suite;
 use crate::run::Runner;
+use anyhow::Result;
 use search_service::LiveSearchService;
 use std::thread::sleep;
 use std::time::Duration;
 
+fn run_application() -> Result<()> {
+    let test_suite = load_suite()?;
+    let search_service = LiveSearchService::new(test_suite.endpoint().clone());
+    let runner = Runner::new(&test_suite, search_service)?;
+
+    println!("{}", test_suite.headline());
+
+    for result in runner.run() {
+        print!("{result}");
+        sleep(Duration::from_millis(test_suite.delay()));
+    }
+
+    Ok(())
+}
+
 fn main() {
-    let test_suite = load_suite();
-
-    match test_suite {
-        Ok(test_suite) => {
-            let search_service = LiveSearchService::new(test_suite.endpoint().clone());
-            let runner = Runner::new(&test_suite, search_service).unwrap();
-
-            println!("{}", test_suite.headline());
-
-            for result in runner.run() {
-                print!("{result}");
-                sleep(Duration::from_millis(test_suite.delay()));
-            }
-        }
-        Err(error) => println!("{error}"),
+    if let Err(error) = run_application() {
+        println!("{error}")
     }
 }
