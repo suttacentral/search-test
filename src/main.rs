e363d9346ec2a59126281dd784e97f874ae67462
jr <jhanarato@gmail.com>
2025-10-12 18:07:05 +0800

    Move completed ResultCount to new module and use in main()

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod file_load;
 mod identifiers;
 mod report;
 mod response;
+mod result_count;
 mod run;
 mod search_service;
 mod test_case;
@@ -12,90 +13,34 @@ mod test_result;
 mod test_suite;
 
 use crate::file_load::load_suite;
+use crate::result_count::ResultCount;
 use crate::run::Runner;
-use crate::test_result::{Summary, TestResult};
 use anyhow::Result;
 use search_service::LiveSearchService;
 use std::thread::sleep;
 use std::time::Duration;
 
-#[derive(Debug, Clone, PartialEq)]
-struct ResultCount {
-    passed: usize,
-    failed: usize,
-    error: usize,
-}
-
-impl ResultCount {
-    fn new() -> Self {
-        Self {
-            passed: 0,
-            failed: 0,
-            error: 0,
-        }
-    }
-
-    fn add(&mut self, summary: Summary) {
-        match summary {
-            Summary::Passed => self.passed += 1,
-            Summary::Failed => self.failed += 1,
-            Summary::Error => self.error += 1,
-        }
-    }
-}
-
-fn run_application() -> Result<()> {
+fn run_application() -> Result<ResultCount> {
     let test_suite = load_suite()?;
     let search_service = LiveSearchService::new(test_suite.endpoint().clone());
     let runner = Runner::new(&test_suite, search_service)?;
 
     println!("{}", test_suite.headline());
 
+    let mut result_count = ResultCount::new();
+
     for result in runner.run() {
         print!("{result}");
         sleep(Duration::from_millis(test_suite.delay()));
+        result_count.add(&result.outcome.summary());
     }
 
-    Ok(())
+    Ok(result_count)
 }
 
 fn main() {
-    if let Err(error) = run_application() {
-        println!("{error}")
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn initialise_result_count() {
-        let counter = ResultCount::new();
-        assert_eq!(
-            counter,
-            ResultCount {
-                passed: 0,
-                failed: 0,
-                error: 0
-            }
-        );
-    }
-
-    #[test]
-    fn add_one_of_each() {
-        let mut counter = ResultCount::new();
-        counter.add(Summary::Passed);
-        counter.add(Summary::Failed);
-        counter.add(Summary::Error);
-
-        assert_eq!(
-            counter,
-            ResultCount {
-                passed: 1,
-                failed: 1,
-                error: 1,
-            }
-        );
+    match run_application() {
+        Ok(count) => println!("{count}"),
+        Err(error) => println!("{error}"),
     }
 }
---
+++ src/result_count.rs
@@ -0,0 +1,86 @@
+use crate::test_result::Summary;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ResultCount {
+    passed: usize,
+    failed: usize,
+    error: usize,
+}
+
+impl ResultCount {
+    pub fn new() -> Self {
+        Self {
+            passed: 0,
+            failed: 0,
+            error: 0,
+        }
+    }
+
+    pub fn add(&mut self, summary: &Summary) {
+        match summary {
+            Summary::Passed => self.passed += 1,
+            Summary::Failed => self.failed += 1,
+            Summary::Error => self.error += 1,
+        }
+    }
+}
+
+impl Display for ResultCount {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} passed, {} failed, {} encountered an error",
+            self.passed, self.failed, self.error
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn initialise_result_count() {
+        let counter = ResultCount::new();
+        assert_eq!(
+            counter,
+            ResultCount {
+                passed: 0,
+                failed: 0,
+                error: 0
+            }
+        );
+    }
+
+    #[test]
+    fn add_one_of_each() {
+        let mut counter = ResultCount::new();
+        counter.add(&Summary::Passed);
+        counter.add(&Summary::Failed);
+        counter.add(&Summary::Error);
+
+        assert_eq!(
+            counter,
+            ResultCount {
+                passed: 1,
+                failed: 1,
+                error: 1,
+            }
+        );
+    }
+
+    #[test]
+    fn display() {
+        let result_count = ResultCount {
+            passed: 1,
+            failed: 2,
+            error: 0,
+        };
+
+        assert_eq!(
+            result_count.to_string(),
+            "1 passed, 2 failed, 0 encountered an error"
+        )
+    }
+}
