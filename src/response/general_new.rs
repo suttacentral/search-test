63411b184c08b670b9098689dff553c8eb73290a
jr <jhanarato@gmail.com>
2025-11-19 21:09:48 +0800

    Break up general search results with texts() function.
    Issue #3

--- src/response/general_new.rs
+++ src/response/general_new.rs
@@ -1,20 +1,25 @@
-use crate::identifiers::{DictionaryUrl, SuttaplexUid, TextUrl};
-use anyhow::{Error, Result};
+use crate::identifiers::TextUrl;
+use anyhow::Result;
 
-#[derive(Clone, Debug, Default, PartialEq)]
-pub struct GeneralSearchResults {
-    pub text: Vec<TextUrl>,
-    pub dictionary: Vec<DictionaryUrl>,
-    pub fuzzy_dictionary: Vec<DictionaryUrl>,
-    pub suttaplex: Vec<SuttaplexUid>,
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, PartialEq)]
+pub struct Text {
+    uid: String,
+    lang: String,
+    author_uid: Option<String>,
+    url: TextUrl,
 }
 
-impl TryFrom<&str> for GeneralSearchResults {
-    type Error = Error;
+#[derive(Deserialize, Debug)]
+pub struct TopLevel {
+    hits: Vec<Text>,
+}
 
-    fn try_from(_value: &str) -> Result<Self> {
-        Ok(GeneralSearchResults::default())
-    }
+fn texts(json: &str) -> Result<Vec<TextUrl>> {
+    let top_level: TopLevel = serde_json::from_str(json)?;
+    let urls = top_level.hits.iter().map(|hit| hit.url.clone()).collect();
+    Ok(urls)
 }
 
 #[cfg(test)]
@@ -25,19 +30,17 @@ mod tests {
     fn single_text_result() {
         let json = r#"
         {
-            "uid": "sa264",
-            "lang": "en",
-            "author_uid": "analayo",
-            "url": "/sa264/en/analayo"
+            "hits": [
+                {
+                    "uid": "mn1",
+                    "lang": "en",
+                    "author_uid": "sujato",
+                    "url": "/mn1/en/sujato"
+                }
+            ]
         }
         "#;
 
-        assert_eq!(
-            GeneralSearchResults::try_from(json).unwrap(),
-            GeneralSearchResults {
-                text: vec![],
-                ..GeneralSearchResults::default()
-            },
-        )
+        assert_eq!(texts(json).unwrap(), vec![TextUrl::from("/mn1/en/sujato")])
     }
 }
