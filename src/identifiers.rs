5e8b05279d1a54a418ac824877c95b88317c4e6f
jr <jhanarato@gmail.com>
2025-11-21 17:45:43 +0800

    Test SearchType from SearchResultKey
    Issue #3

--- src/identifiers.rs
+++ src/identifiers.rs
@@ -83,3 +83,40 @@ impl From<SearchResultKey> for SearchType {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_type_from_text_key() {
+        let text_key = SearchResultKey::Text {
+            url: TextUrl::from("/mn1/en/sujato"),
+        };
+        assert_eq!(SearchType::from(text_key), SearchType::Text);
+    }
+
+    #[test]
+    fn search_type_from_dictionary_key() {
+        let dictionary_key = SearchResultKey::Dictionary {
+            url: DictionaryUrl::from("/define/metta"),
+        };
+        assert_eq!(SearchType::from(dictionary_key), SearchType::Dictionary);
+    }
+
+    #[test]
+    fn search_type_from_suttaplex_key() {
+        let suttaplex_key = SearchResultKey::Suttaplex {
+            uid: SuttaplexUid::from("mn1"),
+        };
+        assert_eq!(SearchType::from(suttaplex_key), SearchType::Suttaplex);
+    }
+
+    #[test]
+    fn search_type_from_volpage_key() {
+        let volpage_key = SearchResultKey::Volpage {
+            reference: VolpageReference::from("PTS SN ii 1"),
+        };
+        assert_eq!(SearchType::from(volpage_key), SearchType::Volpage);
+    }
+}
