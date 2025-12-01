479f7d937c5e7bc5c9b91f0d75926a0864d932d3
jr <jhanarato@gmail.com>
2025-12-01 10:46:40 +0800

    Begin implementing TestResult::new_style_results()
    Issue #3

--- src/test_result.rs
+++ src/test_result.rs
@@ -1,9 +1,10 @@
 use crate::outcome::Outcome;
+use crate::response::search_results::SearchResultsNewStyle;
 use crate::test_case::TestCase;
 use crate::timed_response::TimedResponse;
 use crate::timed_search_results::TimedSearchResults;
+use anyhow::Result;
 use std::time::Duration;
-// use crate::response::search_results::SearchResults;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct TestResult {
@@ -14,10 +15,6 @@ pub struct TestResult {
 
 impl TestResult {
     pub fn new(test_case: &TestCase, response: TimedResponse) -> Self {
-        // let search_type = test_case.search_type();
-        // let json = response.json;
-        // let search_results = SearchResults::new(search_type, json);
-
         let timed_search_results = TimedSearchResults::from(response);
 
         Self {
@@ -26,12 +23,26 @@ impl TestResult {
             outcome: Outcome::new(&test_case.expected, &timed_search_results.results),
         }
     }
+
+    fn new_style_results(
+        test_case: &TestCase,
+        json: Result<String>,
+    ) -> Result<SearchResultsNewStyle> {
+        let json = json?;
+        match test_case.search_type() {
+            None => todo!(),
+            Some(search_type) => SearchResultsNewStyle::new(search_type, json.as_str()),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::expected::Expected;
+    use crate::identifiers::{SearchResultKey, SuttaplexUid};
     use crate::test_case::TestCase;
+    use anyhow::anyhow;
     use std::time::Duration;
 
     fn test_case() -> TestCase {
@@ -55,6 +66,31 @@ mod tests {
         "suttaplex" : [ { "uid": "mn1" } ]
     }
     "#;
+
+    #[test]
+    fn new_style_results_when_no_json() {
+        let results =
+            TestResult::new_style_results(&test_case(), Err(anyhow!("Failed to get JSON")))
+                .unwrap_err();
+        assert_eq!(results.to_string(), "Failed to get JSON")
+    }
+
+    #[test]
+    fn new_style_results_when_bad_json() {
+        let test_case = TestCase {
+            expected: Some(Expected::Unranked {
+                key: SearchResultKey::Suttaplex {
+                    uid: SuttaplexUid::from("mn1"),
+                },
+            }),
+            ..test_case()
+        };
+
+        let results =
+            TestResult::new_style_results(&test_case, Ok(String::from("This is not JSON")))
+                .unwrap_err();
+        assert_eq!(results.to_string(), "expected value at line 1 column 1")
+    }
 
     fn ok_response() -> TimedResponse {
         TimedResponse {
