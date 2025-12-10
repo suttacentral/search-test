15592747dbbcfebc6648daa296503cc890064061
jr <jhanarato@gmail.com>
2025-12-10 08:43:51 +0800

    Remove old style search from outcome.rs
    Issue #3

--- src/outcome.rs
+++ src/outcome.rs
@@ -2,7 +2,6 @@ use crate::category_search::CategorySearch;
 use crate::expected::Expected;
 use crate::identifiers::{SearchResultKey, SuttaplexUid};
 use crate::rank::Rank;
-use crate::response::general::SearchResultsOldStyle;
 use crate::response::search_results::SearchResultsNewStyle;
 use anyhow::{Context, Result};
 
@@ -65,48 +64,12 @@ impl Outcome {
             }
         }
     }
-
-    pub fn new_with_old_style_results(
-        expected: &Option<Expected>,
-        search_results: &Result<SearchResultsOldStyle>,
-    ) -> Self {
-        match search_results {
-            Ok(search_results) => Self::success(expected, search_results),
-            Err(error) => Self::Error {
-                message: format!("{error:#}"),
-            },
-        }
-    }
-
-    fn success(expected: &Option<Expected>, search_results: &SearchResultsOldStyle) -> Self {
-        match expected {
-            None => Self::Success,
-            Some(expected) => Self::expected(expected, search_results),
-        }
-    }
-
-    fn expected(expected: &Expected, search_results: &SearchResultsOldStyle) -> Self {
-        match expected {
-            Expected::Unranked { key } => {
-                let search = CategorySearch::new(key, search_results);
-                match search.found() {
-                    true => Outcome::Found { search },
-                    false => Outcome::NotFound { search },
-                }
-            }
-            Expected::Ranked { key, min_rank } => {
-                let search = CategorySearch::new(key, search_results);
-                let rank = Rank::new(*min_rank, search.rank());
-                Outcome::Ranked { search, rank }
-            }
-        }
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::identifiers::{DictionaryUrl, SearchResultKey, SuttaplexUid};
+    use crate::identifiers::{SearchResultKey, SuttaplexUid};
     use crate::test_json::SUTTAPLEX_MN1_JSON;
     use anyhow::anyhow;
 
@@ -254,165 +217,6 @@ mod tests {
                     minimum: 1,
                     actual: 1
                 },
-            }
-        )
-    }
-
-    #[test]
-    fn new_outcome_is_success_when_nothing_expected() {
-        let search_results = SearchResultsOldStyle {
-            text: Vec::new(),
-            dictionary: Vec::new(),
-            suttaplex: Vec::new(),
-        };
-
-        let outcome = Outcome::new_with_old_style_results(&None, &Ok(search_results));
-        assert_eq!(outcome, Outcome::Success);
-    }
-
-    #[test]
-    fn outcome_is_found_in_search() {
-        let expected = Expected::Unranked {
-            key: SearchResultKey::Suttaplex {
-                uid: SuttaplexUid::from("mn1"),
-            },
-        };
-
-        let search_results = SearchResultsOldStyle {
-            text: Vec::new(),
-            dictionary: Vec::new(),
-            suttaplex: vec![SuttaplexUid::from("mn1")],
-        };
-
-        let outcome = Outcome::new_with_old_style_results(&Some(expected), &Ok(search_results));
-
-        assert_eq!(
-            outcome,
-            Outcome::Found {
-                search: CategorySearch::Suttaplex {
-                    search_for: SuttaplexUid::from("mn1"),
-                    in_results: vec![SuttaplexUid::from("mn1")],
-                }
-            }
-        );
-    }
-
-    #[test]
-    fn outcome_is_not_found_in_search() {
-        let expected = Expected::Unranked {
-            key: SearchResultKey::Suttaplex {
-                uid: SuttaplexUid::from("mn1"),
-            },
-        };
-
-        let search_results = SearchResultsOldStyle {
-            text: Vec::new(),
-            dictionary: Vec::new(),
-            suttaplex: vec![SuttaplexUid::from("mn2")],
-        };
-
-        let outcome = Outcome::new_with_old_style_results(&Some(expected), &Ok(search_results));
-
-        assert_eq!(
-            outcome,
-            Outcome::NotFound {
-                search: CategorySearch::Suttaplex {
-                    search_for: SuttaplexUid::from("mn1"),
-                    in_results: vec![SuttaplexUid::from("mn2")],
-                }
-            }
-        );
-    }
-
-    #[test]
-    fn outcome_is_sufficient_rank() {
-        let expected = Expected::Ranked {
-            key: SearchResultKey::Suttaplex {
-                uid: SuttaplexUid::from("mn1"),
-            },
-            min_rank: 1,
-        };
-
-        let search_results = SearchResultsOldStyle {
-            text: Vec::new(),
-            dictionary: Vec::new(),
-            suttaplex: vec![SuttaplexUid::from("mn1"), SuttaplexUid::from("mn2")],
-        };
-
-        let outcome = Outcome::new_with_old_style_results(&Some(expected), &Ok(search_results));
-
-        assert_eq!(
-            outcome,
-            Outcome::Ranked {
-                search: CategorySearch::Suttaplex {
-                    search_for: SuttaplexUid::from("mn1"),
-                    in_results: vec![SuttaplexUid::from("mn1"), SuttaplexUid::from("mn2")]
-                },
-                rank: Rank::Sufficient {
-                    minimum: 1,
-                    actual: 1
-                }
-            }
-        )
-    }
-
-    #[test]
-    fn outcome_rank_is_too_low() {
-        let expected = Expected::Ranked {
-            key: SearchResultKey::Suttaplex {
-                uid: SuttaplexUid::from("mn2"),
-            },
-            min_rank: 1,
-        };
-
-        let search_results = SearchResultsOldStyle {
-            text: Vec::new(),
-            dictionary: Vec::new(),
-            suttaplex: vec![SuttaplexUid::from("mn1"), SuttaplexUid::from("mn2")],
-        };
-
-        let outcome = Outcome::new_with_old_style_results(&Some(expected), &Ok(search_results));
-
-        assert_eq!(
-            outcome,
-            Outcome::Ranked {
-                search: CategorySearch::Suttaplex {
-                    search_for: SuttaplexUid::from("mn2"),
-                    in_results: vec![SuttaplexUid::from("mn1"), SuttaplexUid::from("mn2")]
-                },
-                rank: Rank::TooLow {
-                    minimum: 1,
-                    actual: 2
-                }
-            }
-        )
-    }
-
-    #[test]
-    fn outcome_ranked_but_not_found() {
-        let expected = Expected::Ranked {
-            key: SearchResultKey::Dictionary {
-                url: DictionaryUrl::from("/define/metta"),
-            },
-            min_rank: 1,
-        };
-
-        let search_results = SearchResultsOldStyle {
-            text: Vec::new(),
-            dictionary: vec![DictionaryUrl::from("/define/dosa")],
-            suttaplex: Vec::new(),
-        };
-
-        let outcome = Outcome::new_with_old_style_results(&Some(expected), &Ok(search_results));
-
-        assert_eq!(
-            outcome,
-            Outcome::Ranked {
-                search: CategorySearch::Dictionary {
-                    search_for: DictionaryUrl::from("/define/metta"),
-                    in_results: vec![DictionaryUrl::from("/define/dosa")],
-                },
-                rank: Rank::NotFound { minimum: 1 }
             }
         )
     }
